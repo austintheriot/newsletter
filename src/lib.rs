928f0920054d81ab9649ba96d82f165af70c4b5b
Austin Theriot <austinmtheriot@gmail.com>
2022-12-04 16:24:08 -0600

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
+use actix_web::{dev::Server, get, post, web, App, HttpResponse, HttpServer};
 use serde::{Deserialize, Serialize};
 
+#[get("/health_check")]
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
@@ -13,18 +14,15 @@ pub struct SubscribePayload {
     email: String,
 }
 
+#[post("/subscribe")]
 async fn subscribe(_json: web::Json<SubscribePayload>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscribe", web::post().to(subscribe))
-    })
-    .listen(listener)?
-    .run();
+    let server = HttpServer::new(|| App::new().service(health_check).service(subscribe))
+        .listen(listener)?
+        .run();
 
     Ok(server)
 }
