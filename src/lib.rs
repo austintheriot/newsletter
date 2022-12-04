d6f1126b4ad6e25c2fdf0abcc308284dee997ff6
Austin Theriot <austinmtheriot@gmail.com>
2022-12-04 15:36:32 -0600

    stub out /subscribe endpoint with tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,30 @@
 use std::net::TcpListener;
 
 use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
+use serde::{Deserialize, Serialize};
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct SubscribePayload {
+    name: String,
+    email: String,
+}
+
+async fn subscribe(_json: web::Json<SubscribePayload>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscribe", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 
 use newsletter_api::run;
+use serde_json::json;
 
 #[tokio::test]
 async fn health_check_works() {
@@ -8,7 +9,7 @@ async fn health_check_works() {
     let client = reqwest::Client::new();
 
     let response = client
-        .get(address)
+        .get(format!("{address}/health_check"))
         .send()
         .await
         .expect("Failed to execute request");
@@ -17,12 +18,71 @@ async fn health_check_works() {
     assert_eq!(response.content_length(), Some(0));
 }
 
+#[tokio::test]
+async fn subscribe_returns_200_for_valid_form_data_json() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let payload = json!({
+        "name": "Austin",
+        "email": "test@gmail.com",
+    });
+
+    let response = client
+        .post(format!("{address}/subscribe"))
+        .header("Content-Type", "application/json")
+        .body(payload.to_string())
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert!(response.status().is_success());
+    assert_eq!(response.content_length(), Some(0));
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_for_invalid_form_data_json() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let payloads = [
+        json!({
+            "cat": "Meow",
+            "dog": "woof@gmail.com",
+        })
+        .to_string(),
+        json!({
+            "name": "Austin",
+        })
+        .to_string(),
+        json!({
+            "email": "test@gmail.com",
+        })
+        .to_string(),
+        String::from(""),
+    ];
+
+    for payload in payloads {
+        let response = client
+            .post(format!("{address}/subscribe"))
+            .header("Content-Type", "application/json")
+            .body(payload.to_string())
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            response.status().as_u16(),
+            400,
+            "The API did not fail with 400 Bad Request when the payload was {payload}"
+        );
+    }
+}
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let server = run(listener).expect("Failed to bind address");
 
     let _handle = tokio::spawn(server);
-    let address = format!("http://127.0.0.1:{}/health_check", port);
+    let address = format!("http://127.0.0.1:{}", port);
     address
 }
