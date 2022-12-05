0d4deddeaf0791745d662e2760ed3c89813f48e1
Austin Theriot <austinmtheriot@gmail.com>
2022-12-05 12:45:07 -0600

    print address the server is running at

--- src/lib.rs
+++ src/lib.rs
@@ -20,9 +20,15 @@ async fn subscribe(_json: web::Json<SubscribePayload>) -> HttpResponse {
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let socket_addr = listener.local_addr();
+
     let server = HttpServer::new(|| App::new().service(health_check).service(subscribe))
         .listen(listener)?
         .run();
+
+    if let Ok(local_addr) = socket_addr {
+        println!("Running server at: http://{local_addr}");
+    }
 
     Ok(server)
 }
