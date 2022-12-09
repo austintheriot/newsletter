43100f6f53a090a95641305d3dbbefc810b6cab7
Austin Theriot <austinmtheriot@gmail.com>
2022-12-08 21:18:56 -0600

    enable configuration files

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::net::TcpListener;
 
-use newsletter_api::startup::run;
+use newsletter_api::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
